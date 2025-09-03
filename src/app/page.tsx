b556a156066eaeff57952cdc4bd5b6fc1b0a7a69
João Demutti <joaofacindemutti@gmail.com>
2025-09-02 22:03:36 -0300

    circles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,21 @@ export default function Page() {
   return (
     <div className="space-y-8 p-8">
 
-      <div className="neomorph size-3.5 rounded-full inline-flex">
+      
+      <div className="flex justify-between items-center">
+
+        <span className="neomorph size-3.5 rounded-full inline-flex">
+        </span>
+
+        <span className="neomorph size-7 rounded-full inline-flex">
+        </span>
+
+        <span className="neomorph size-11 rounded-full inline-flex">
+        </span>
+
+        <span className="neomorph size-17 rounded-full inline-flex">
+        </span>
+
       </div>
       
       <div className="gradient neomorph">
